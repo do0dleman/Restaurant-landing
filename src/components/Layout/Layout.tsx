628d2ad749bf7cd6c41b1d2d5f3afbf09d4954cd
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-03-14 15:57:57 +0200

    added animation on page change

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,23 @@
+import { AnimationEventHandler } from "react";
 import { Outlet } from "react-router";
 import Footer from "../../modules/footer/Footer";
 import Navbar from "../../modules/navbar/Navbar";
 
-export default function Layout() {
+interface LayoutProps {
+    transitionStage: string
+    onAnimationEnd: AnimationEventHandler<HTMLDivElement>
+}
+export default function Layout(props: LayoutProps) {
+    const { transitionStage, onAnimationEnd } = props
+    console.log(transitionStage)
     return (
         <>
             <Navbar />
-            <Outlet />
+            <div
+                className={`content ${transitionStage}`}
+                onAnimationEnd={onAnimationEnd}>
+                <Outlet />
+            </div>
             <Footer />
         </>
     )
--- src/components/Layout/Router.tsx
+++ src/components/Layout/Router.tsx
@@ -1,23 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import About from "../../pages/About";
 import ContactUs from "../../pages/ContactUs";
 import Menu from "../../pages/Menu";
 import Reservations from "../../pages/Reservations";
 import Welcome from "../../pages/Welcome";
 import Layout from "./Layout";
+import './animation.scss'
 
 export default function Router() {
+    const location = useLocation()
+    const [displayLocation, setDisplayLocation] = useState(location)
+    const [transitionStage, setTransitionStage] = useState('fade-in')
+    useEffect(() => {
+        // setDisplayLocation(location)
+        setTransitionStage('fade-out')
+    }, [location])
+    function HandleAnimationEnd() {
+        setTransitionStage('fade-in')
+        setDisplayLocation(location)
+    }
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Layout />}>
-                    <Route index element={<Welcome />} />
-                    <Route path='menu' element={<Menu />} />
-                    <Route path='reservations' element={<Reservations />} />
-                    <Route path='about' element={<About />} />
-                    <Route path='contact' element={<ContactUs />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes location={displayLocation}>
+            <Route path='/' element={
+                <Layout
+                    onAnimationEnd={HandleAnimationEnd}
+                    transitionStage={transitionStage}
+                />}>
+                <Route index element={<Welcome />} />
+                <Route path='menu' element={<Menu />} />
+                <Route path='reservations' element={<Reservations />} />
+                <Route path='about' element={<About />} />
+                <Route path='contact' element={<ContactUs />} />
+            </Route>
+        </Routes>
     )
 }
